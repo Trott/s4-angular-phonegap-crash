b655ef2a9d6ad6f743b2b0708dcdfae6ce4b5c89
Rich Trott <rtrott@gmail.com>
2013-10-17 15:24:31 -0700

    rm createCORSRequest()

--- www/js/ucsf.js
+++ www/js/ucsf.js
@@ -3,44 +3,10 @@ var UCSF = UCSF || (function () {
 
     var me = {
 
-        createCORSRequest: function (method, url, success, failure) {
-            var xhr = new window.XMLHttpRequest();
-            if ("withCredentials" in xhr) {
-                // XHR for Chrome/Firefox/Opera/Safari.
-                xhr.open(method, url);
-            } else if (typeof XDomainRequest !== "undefined") {
-                // XDomainRequest for IE8+.
-                xhr = new XDomainRequest();
-                xhr.open(method, url);
-            } else if ((typeof flensed !== "undefined") && "flXHR" in flensed) {
-                // flensed.flXHR polyfill for IE7
-                xhr = new flensed.flXHR({
-                    xmlResponseText:false,
-                    onreadystatechange:
-                        function (XHRobj) {
-                            if (XHRobj.readyState === 4) {
-                                if (XHRobj.status === 200 && success) {
-                                    success(JSON.parse(XHRobj.responseText));
-                                } else {
-                                    failure(XHRobj);
-                                }
-                            }
-                        },
-                    ontimeout: failure,
-                    onerror: failure
-                });
-                xhr.open(method, url, true);
-            } else {
-                xhr = null;
-            }
-            return xhr;
-        },
-
         run: function (what, success, failure) {
             failure = failure || function (obj) {window.alert(obj.statusText||'An error occurred. Please try again.');};
-            var reqString = 'http://apis.ucsf.edu/' + what;
-            var xhr = UCSF.createCORSRequest('GET', reqString, success, failure);
-
+            var xhr = new window.XMLHttpRequest();
+            xhr.open('GET', 'http://apis.ucsf.edu/shuttle/routes');
             xhr.onload = function () {
                 success(JSON.parse(xhr.responseText));
             };
