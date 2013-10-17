19f5e08b20c985a51d13072635b55234fcc29d8d
Rich Trott <rtrott@gmail.com>
2013-10-17 15:27:16 -0700

    rm layers

--- www/js/ucsf.js
+++ www/js/ucsf.js
@@ -1,30 +1,13 @@
-var UCSF = UCSF || (function () {
-    "use strict";
-
-    var me = {
-
-        run: function (what, success, failure) {
-            failure = failure || function (obj) {window.alert(obj.statusText||'An error occurred. Please try again.');};
-            var xhr = new window.XMLHttpRequest();
-            xhr.open('GET', 'http://apis.ucsf.edu/shuttle/routes');
-            xhr.onload = function () {
-                success(JSON.parse(xhr.responseText));
-            };
-            xhr.onerror=failure;
-
-            xhr.send(null);
-
-        }
+var UCSF = {};
+UCSF.Shuttle = {};
+UCSF.Shuttle.routes = function(success, failure) {
+    failure = failure || function (obj) {window.alert(obj.statusText||'An error occurred. Please try again.');};
+    var xhr = new window.XMLHttpRequest();
+    xhr.open('GET', 'http://apis.ucsf.edu/shuttle/routes');
+    xhr.onload = function () {
+        success(JSON.parse(xhr.responseText));
     };
+    xhr.onerror=failure;
 
-    return me;
-}());
-
-UCSF.Shuttle = (function() {
-
-    return {
-        routes: function (success, failure) {
-            UCSF.run('shuttle/routes', success, failure, UCSF.Shuttle.routes);
-        }
-    };
-}());
+    xhr.send(null);
+};
