f296b14a81e6302343fa75cdae1c13231ccaf400
Rich Trott <rtrott@gmail.com>
2013-10-17 09:59:05 -0700

    rm code not needed to replicate crash

--- www/js/modules/main.js
+++ www/js/modules/main.js
@@ -1,19 +1,9 @@
 (function () {
     'use strict';
     angular.module('main', ['news'])
-    .config(['$compileProvider', function ($compileProvider) {
-        $compileProvider.urlSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
-    }])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
         .when('/', {templateUrl: 'main/mainMenu.html'})
         .otherwise({redirectTo: '/'});
-    }])
-    .controller('navigationController', ['$scope', '$location', function ($scope, $location) {
-        var getHideNavigation = function () {
-            return $location.path() === '/';
-        };
-
-        $scope.$watch(getHideNavigation, function() { $scope.hideNavigation = getHideNavigation(); });
     }]);
 }());
