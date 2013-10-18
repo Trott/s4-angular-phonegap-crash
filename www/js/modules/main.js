6abd046c1e1192f07da2782c17f0b1f7d520bdcf
Rich Trott <rtrott@gmail.com>
2013-10-17 23:10:32 -0700

    rm fastclick

--- www/js/modules/main.js
+++ www/js/modules/main.js
@@ -3,7 +3,12 @@
     angular.module('main', ['list'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
-        .when('/', {templateUrl: 'main'})
+        .when('/', {templateUrl: 'main', controller: 'mainController'})
         .otherwise({redirectTo: '/'});
+    }]).
+    controller('mainController', ['$scope', '$location', function ($scope, $location) {
+        $scope.showList = function () {
+            $location.path('/list');
+        };
     }]);
 }());
